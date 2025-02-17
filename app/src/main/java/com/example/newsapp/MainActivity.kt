0e4267c8f74ab425336abf2debd8c350d0fc14c4
Ru7tam <105158227+Ru7tam@users.noreply.github.com>
2025-02-17 16:31:05 +0300

    set up viewModel and a little navigation

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -1,43 +1,38 @@
 package com.example.newsapp
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
-import androidx.lifecycle.lifecycleScope
-import com.example.newsapp.domain.usecases.AppEntryUseCases
+import com.example.newsapp.presentation.nvgraph.NavGraph
 import com.example.newsapp.ui.theme.NewsAppTheme
-import com.loc.newsapp.presentation.onboarding.OnBoardingScreen
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    @Inject
-    lateinit var appEntryUseCases: AppEntryUseCases
+
+    val viewModel by viewModels<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
-        installSplashScreen()
-        lifecycleScope.launch {
-            appEntryUseCases.readAppEntry().collect {
-                Log.d("Test", it.toString())
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                viewModel.splashCondition
             }
         }
+
         setContent {
-            NewsAppTheme(
-                dynamicColor = false
-            ) {
+            NewsAppTheme {
                 Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)) {
-                    OnBoardingScreen()
+                    val startDestination = viewModel.startDestination
+                    NavGraph(startDestination = startDestination)
                 }
             }
         }
---
+++ app/src/main/java/com/example/newsapp/presentation/onBoarding/OnBoardingViewModel.kt
@@ -0,0 +1,31 @@
+package com.example.newsapp.presentation.onBoarding
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.newsapp.domain.usecases.AppEntryUseCases
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class OnBoardingViewModel @Inject constructor(
+    private val appEntryUseCases: AppEntryUseCases
+) : ViewModel() {
+
+    fun onEvent(event: OnBoardingEvent) {
+        when (event) {
+            is OnBoardingEvent.SaveAppEntry -> {
+                saveUserEntry()
+            }
+        }
+    }
+
+    private fun saveUserEntry() {
+        viewModelScope.launch {
+            appEntryUseCases.saveAppEntry
+        }
+
+    }
+
+
+}
